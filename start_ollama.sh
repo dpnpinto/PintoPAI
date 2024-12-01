42b8746c15d686fd241a34702cf0d3d1e4312dad
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-12-01 20:30:59 -0100

    Create start_ollama.sh

---
+++ start_ollama.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+figlet PintoPAI
+echo "-----------------------------------------"
+echo "# Pinto Personal Artificial Inteligence #"
+echo "-----------------------------------------"
+echo START OLLAMA
+sudo systemctl start ollama.service
