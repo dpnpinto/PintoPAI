2941c6bbb2901e8ac8726396b08a46d0ed4016a4
Duarte Pedro Pinto <49436188+dpnpinto@users.noreply.github.com>
2024-12-01 20:32:15 -0100

    Create stop_ollama.sh

---
+++ stop_ollama.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+figlet PintoPAI
+echo "-----------------------------------------"
+echo "# Pinto Personal Artificial Inteligence #"
+echo "-----------------------------------------"
+echo STOP OLLAMA
+sudo systemctl stop ollama.service
